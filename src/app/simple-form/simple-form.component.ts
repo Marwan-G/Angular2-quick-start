56e3171789a5876dd1c3d9545c4d905f78789aed
Marwan-G <ghabin2004@gmail.com>
2017-06-29 22:27:45 +0200

    elements with event and ref

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
-  template: '<div><p>test</p></div>',
+  templateUrl: './simple-form.component.html',
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+  onClick(value){
+    console.log(value);
+  }
 
   constructor() { }
 
