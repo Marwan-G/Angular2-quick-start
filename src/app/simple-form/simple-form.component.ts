c39c533fbf67441e4d4637c1566613fcf019aca4
Marwan-G <ghabin2004@gmail.com>
2017-06-29 19:54:28 +0200

    create simple Angular component

---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  template: '<div><p>test</p></div>',
+  styles: []
+})
+export class SimpleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
