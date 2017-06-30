8fa8ed107ae50342c2786a1fe6bc45d6368a678f
Marwan-G <ghabin2004@gmail.com>
2017-06-30 20:36:09 +0200

    dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {MailService} from './mail.service';
 
 @Component({
   selector: 'app-root',
   template: `<div>
   <app-simple-form></app-simple-form>
+  <h2>{{mail.message}} </h2>
   </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hey Let Us Understand Angular2!';
+  constructor(private mail:MailService){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {MailService} from './mail.service';
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
 
@@ -16,7 +16,7 @@ import { SimpleFormComponent } from './simple-form/simple-form.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [MailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
