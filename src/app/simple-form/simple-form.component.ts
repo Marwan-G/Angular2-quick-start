99d185dc006e1a641c85e0447f58822c828581c7
Marwan-G <ghabin2004@gmail.com>
2017-07-05 00:07:08 +0200

    Writing CSS inside Angular2 Component

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,7 +3,19 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   templateUrl: './simple-form.component.html',
-  styles: []
+  styles: [`
+    :host{
+    display:flex;
+    flex-direction: column;
+    }
+    *{
+    font-family: monospace;
+    }
+    input:focus{
+    font-weight: bold;
+    outline: none;
+    }
+  `]
 })
 export class SimpleFormComponent implements OnInit {
   @Input() receivedMessage;
