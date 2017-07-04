49e7a557e6585606c82ccd302184728cb9ec717b
Marwan-G <ghabin2004@gmail.com>
2017-07-04 23:45:58 +0200

    Pass events from Angular2 component with Output

--- src/app/mail.service.ts
+++ src/app/mail.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MailService {
-messages = [' heeey Man Marwan how are you ', 'fine and you', 'yes, im doing well with angular'];
-  constructor() { }
-
+messages = [
+  { id:0,text:'heeey Man Marwan how are you '},
+  { id:1,text:'fine and ,you'},
+  { id:2,text:'thanks , things getting well '}];
+update(id, text)
+{
+  this.messages=this.messages.map((m)=>
+  m.id===id
+  ?{id, text}
+  :m
+  )
 }
+
+  constructor() { }
+}
+
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
@@ -7,12 +7,8 @@ import {Component, OnInit, Input} from '@angular/core';
 })
 export class SimpleFormComponent implements OnInit {
   @Input() receivedMessage;
-  onClick(event, value){
-    console.log(value);
-    console.log(event);
-  }
+  @Output() update = new EventEmitter();
   constructor() {
-    setInterval(()=>this.receivedMessage= Math.random().toString(),2000);
   }
   ngOnInit() {
   }
